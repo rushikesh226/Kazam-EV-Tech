2bf88a20791a7443af242634226f4cb9eb8b6a73
rushikesh226 <rushikeshmali226@gmail.com>
2025-02-06 15:22:17 +0530

    env

--- kazam-backend/Routes/user.js
+++ kazam-backend/Routes/user.js
@@ -1,5 +1,6 @@
 const router=require("express").Router();
-const User = require("../Models/user")
+const User = require("../Models/user");
+const bcrypt=require("bcryptjs");
 router.post("/sign-in",async(req,res)=>{
     try {
         const {username}=req.body;
@@ -16,7 +17,9 @@ router.post("/sign-in",async(req,res)=>{
         return res.status(400).json({message:"Email Already Exists"})
     }
 
-    const newUser=new User({username:req.body.username,email:req.body.email,password:req.body.password,})
+    const hashPass=await bcrypt.hash(req.body.password,10)
+
+    const newUser=new User({username:req.body.username,email:req.body.email,password:hashPass,})
 
     await newUser.save();
     return res.status(200).json({message:"SignIn Successful"})
